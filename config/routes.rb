f2d8a895084c6cb4f5daa6bed00ebcd303fc5083
xiaobin <xmonkeyreg@gmail.com>
2012-12-04 12:20:39 +0800

    Create a new Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CourseProject::Application.routes.draw do
   #root to: "main#index"
   root to: "posts#index"
-  resources :posts, :only => [:index, :show]
+  resources :posts, :only => [:index, :show] do
+    resources :comments, :only => :create
+  end
 end
